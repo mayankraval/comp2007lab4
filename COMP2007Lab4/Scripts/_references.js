c1491e0e588356aa5d42d32a6e0554af9726e256
mayankraval <mayank.raval8293@gmail.com>
2016-07-08 20:28:38 -0400

    lab4 completed

---
+++ COMP2007Lab4/Scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="jquery-2.2.2.js" />
+/// <autosync enabled="true" />
+/// <reference path="bootstrap.min.js" />
+/// <reference path="jquery.validate.min.js" />
+/// <reference path="jquery.validate.unobtrusive.js" />
+/// <reference path="jquery-2.2.2.min.js" />
